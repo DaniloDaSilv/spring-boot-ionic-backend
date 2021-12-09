eae1d8ff0141fa516ea1ede2c2aa23cfe0595ae9
Danilo da Silva <danilodasilvadisanto@gmail.com>
2021-12-09 16:42:36 -0300

    Estado, cidade, endereço, cliente, tipo cliente e telefones

---
+++ src/main/java/com/danilods/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.danilods.cursomc.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danilods.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+
+}
---
+++ src/main/java/com/danilods/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.danilods.cursomc.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danilods.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+
+}
